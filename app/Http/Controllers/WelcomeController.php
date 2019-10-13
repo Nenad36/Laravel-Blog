e82c74c3cd98aebec0631010543f8648a7672b5b
Nenad36 <nenad.pesic32@gmail.com>
2019-10-13 15:33:18 +0200

    Laravel Blog

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use App\Post;
+use App\Tag;
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function index() {
+
+//        $search = request()->query('search');
+//
+//        if ($search) {
+//
+//            $posts = Post::where('title', 'LIKE', "%{$search}%")->simplePaginate(10);
+//
+//        } else {
+//
+//            $posts = Post::simplePaginate(10);
+//
+//        }
+
+        return view('welcome')
+
+        ->with('categories', Category::all())
+
+        ->with('tags', Tag::all())
+
+        ->with('posts', Post::searched()->simplePaginate(8));
+    }
+}
